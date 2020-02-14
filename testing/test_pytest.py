0f2f1585056ed3fff542b80058b6b07b18028333
Janardhan <jnaidu023@gmail.com>
2020-02-14 10:59:58 +0000

    Junit test cases adding

---
+++ testing/test_pytest.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+option=Options()
+option.add_argument("--headless")
+option.add_argument("--no-sandbox")
+option.add_argument("start-maximized")
+option.add_argument("disable-infobars")
+option.add_argument('--disable-extensions')
+option.add_argument('--disable-dev-shm-usage')
+driver=webdriver.Chrome('',chrome_options=option)
+driver.implicitly_wait(5)
+driver.get('http://104.154.203.199:8080/')
+driver.maximize_window()
+print(driver.title)
+assert driver.title=='PetClinic :: a Spring Framework demonstration','Title not matched'
+print("Login Test successfull")
+driver.close()driver.quit()
