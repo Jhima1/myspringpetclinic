f5ea5e38c5ed27a7a4bf21d8851c5a62500d5993
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-20 20:37:56 +0530

    Update test_pytest.py

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -15,7 +15,7 @@ driver.get('http://192.168.56.120:8083')
 driver.implicitly_wait(10) 
 driver.maximize_window()
 print(driver.title)
-assert driver.title=="PetClinic :: a Spring Framework demonstration","Title not matched"
+assert (driver.title=='PetClinic :: a Spring Framework demonstration'), 'Title not matched'
 driver.implicitly_wait(10)
 print("Login Test successfull")
 driver.close()
