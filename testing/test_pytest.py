70c9b77a82d1eb6bf6ae584651efb48af5484a03
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-20 16:06:52 +0530

    Update test_pytest.py

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -1,4 +1,4 @@
-from selenium import web driver
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 option=Options()
 option.add_argument("--headless")
@@ -7,9 +7,9 @@ option.add_argument("start-maximized")
 option.add_argument("disable-infobars")
 option.add_argument('--disable-extensions')
 option.add_argument('--disable-dev-shm-usage')
-#driver=webdriver.Chrome('./testing/chromedriver',chrome_options=option)
+driver=webdriver.Chrome('./testing/chromedriver',chrome_options=option)
 #driver=webdriver.Chrome("/home/devops/Capstone/testing/chromedriver.exe", options=option)
-driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe")
+#driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe")
 driver.implicitly_wait(5)                         
 driver.get('http://localhost:83')
 driver.maximize_window()
