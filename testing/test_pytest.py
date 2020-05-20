361bffa2513b0a0723062512d5cfcc67f381019b
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-20 21:23:48 +0530

    Update test_pytest.py

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -8,7 +8,7 @@ option.add_argument("disable-infobars")
 option.add_argument('--disable-extensions')
 option.add_argument('--disable-dev-shm-usage')
 
-def test_title_check():
+def initial_check():
     global driver
     driver=webdriver.Chrome("./testing/chromedriver", options=option)
     #driver=webdriver.Chrome("/home/devops/Capstone/testing/chromedriver.exe", options=option)
@@ -18,6 +18,8 @@ def test_title_check():
     #driver.implicitly_wait(10) 
     driver.maximize_window()
     print(driver.title)
+    
+def title_check():
     #assert (driver.title=='PetClinic :: a Spring Framework demonstration'), 'Title not matched'
     #driver.implicitly_wait(10)
     assert (driver.title == 'PetClinic :: a Spring Framework demonstration'), 'title not matched'
