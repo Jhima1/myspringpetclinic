7f34adf2f3f6aaedad0b4e94ae752cc9e96d7095
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-21 19:32:08 +0530

    Update chrome_get_title.py

--- testing/chrome_get_title.py
+++ testing/chrome_get_title.py
@@ -13,10 +13,7 @@ try:
     print("Attempt")
     time.sleep(5)
     driver.get('http://localhost:8083')
-    print("Successfull")
-    #time.sleep(10)
-    #assert 'PetClinic :: a Spring Framework demonstration' == driver.title
-    #print("Successfull")
-
+    print("PetClinic Successfully Launched")
+    
 finally:
     driver.quit()
