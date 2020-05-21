2e3565753494bc2b7b1143211ba34435d49f2de2
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-21 19:37:01 +0530

    Update test_pytest.py

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -1,31 +1,19 @@
-from selenium import webdriver
+#!/usr/bin/env python3
+
+from selenium.webdriver import Chrome
 import time
 from selenium.webdriver.chrome.options import Options
-option=Options()
-option.add_argument("--headless")
-option.add_argument("--no-sandbox")
-option.add_argument("start-maximized")
-option.add_argument("disable-infobars")
-option.add_argument('--disable-extensions')
-option.add_argument('--disable-dev-shm-usage')
 
-def initial_check():
-    global driver
-    driver=webdriver.Chrome("./testing/chromedriver", options=option)
-    #driver=webdriver.Chrome("/home/devops/Capstone/testing/chromedriver.exe", options=option)
-    #driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe")
-    driver.implicitly_wait(15)
-    time.sleep(10)
-    driver.get('http://192.168.56.120:8083')
-    #driver.implicitly_wait(10) 
-    driver.maximize_window()
-    print(driver.title)
-    #assert 'PetClinic :: a Spring Framework demonstration' == driver.title
-    print("Success")
-    #assert (driver.title=='PetClinic :: a Spring Framework demonstration'), 'Title not matched'
-    #driver.implicitly_wait(10)
-    #assert (driver.title == 'PetClinic :: a Spring Framework demonstration'), 'title not matched'
-    driver.close()
-    #print("Login Test successfull")
-    #driver.close()
-    #driver.quit()
+opts = Options()
+opts.headless = True
+
+driver = Chrome(options=opts, executable_path='./testing/chromedriver')
+
+try:
+    print("Attempt")
+    time.sleep(5)
+    driver.get('http://localhost:8083')
+    print("PetClinic Successfully Launched")
+    
+finally:
+    driver.quit()
