a85f9b740a38ce2659fb2aec14915f9e452f4f03
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-21 12:26:24 +0530

    Create chrome_get_title.py

---
+++ testing/chrome_get_title.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+opts = Options()
+opts.headless = True
+
+driver = Chrome(options=opts, executable_path='./testing/chromedriver')
+
+try:
+    driver.get('http://localhost:8083')
+
+    assert 'PetClinic :: a Spring Framework demonstration' == driver.title
+
+finally:
+
+    driver.quit()
