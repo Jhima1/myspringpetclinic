8ac12c36f195a73469e6becfceca1348ad855b71
Jhima1 <48076892+Jhima1@users.noreply.github.com>
2020-05-21 18:46:53 +0530

    Update chrome_get_title.py

--- testing/chrome_get_title.py
+++ testing/chrome_get_title.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from selenium.webdriver import Chrome
+import time
 from selenium.webdriver.chrome.options import Options
 
 opts = Options()
@@ -9,8 +10,12 @@ opts.headless = True
 driver = Chrome(options=opts, executable_path='./testing/chromedriver')
 
 try:
+    print("Attempt")
+    time.sleep(5)
     driver.get('http://localhost:8083')
+    time.sleep(10)
     assert 'PetClinic :: a Spring Framework demonstration' == driver.title
+    print("Successfull")
 
 finally:
     driver.quit()
